4c737f136d27b37db16424f444fe8fa6b31b2d0f
Storbod <oscar_englundh@hotmail.com>
2017-05-07 22:32:20 +0200

    Started on a GUI

--- pyCubeStats.py
+++ pyCubeStats.py
@@ -1,12 +1,15 @@
-from timeit import default_timer as timer
-from time import sleep
+from time import sleep, process_time
 from collections import OrderedDict
-import csv
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QIcon
+import csv, sys
 
 #The inspection time in seconds
+#Change it to whatever suits you
 inspectionTime = 15
 
 #The time keys
+#Change it to whatever suits you
 timeKeys = [60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
 
 def writeLatestTime(time):
@@ -14,22 +17,22 @@ def writeLatestTime(time):
 		times.write(str(time) + ",")
 
 def readTimes():
-	return list(csv.reader(open("times.csv", "r"), delimiter=","))[0]
+	times = [time for time in list(csv.reader(open("times.csv", "r")))[0]]
+	times.pop()
+	times = [float(time) for time in times]
+	return times
 
 def runTimer():
-	start = timer()
+	start = process_time()
 	input()
-	end = timer()
-	time = round(end - start, 3)
-	print(str(time))
-	
-	return time
+	return round(process_time() - start, 3)
 
 def inspection(inspectionTime):
 	sleep(inspectionTime)
 
 def stats(times, timeKeys):
 	#Based on code by https://www.reddit.com/user/yovliporat
+	#https://drive.google.com/file/d/0B7qI7oJsiTPGcjY2VlpoQi1hLVU/view
 	dictonary = OrderedDict()
 
 	for value in timeKeys:
@@ -44,5 +47,27 @@ def stats(times, timeKeys):
 	for key, val in dictonary.items():
 		print("Sub-{}: {} solves [{}%]".format(str(key), str(val), str(float(val) / float(len(times)) * 100.0)[:5]))
 
-times = readTimes()
-stats(times, timeKeys)
+class init(QWidget):
+	def __init__(self):
+		super().__init__()
+		self.initUI()
+		
+	def initUI(self):
+		
+		startButton = QPushButton("Start", self)
+		timer = QLCDNumber(self)
+		
+		vbox = QVBoxLayout()
+		vbox.addWidget(timer)
+		vbox.addWidget(startButton)
+		
+		self.setLayout(vbox)
+		
+		self.setGeometry(300, 300, 250, 100)
+		self.setWindowTitle("Python Cube Timer")
+		self.setWindowIcon(QIcon("icon.png"))
+		self.show()
+
+app = QApplication(sys.argv)
+initiation = init()
+sys.exit(app.exec_())
