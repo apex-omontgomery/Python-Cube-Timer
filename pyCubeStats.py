4e3c0904333d51fcba112f0f909a56435d186cfa
Storbod <oscar_englundh@hotmail.com>
2017-05-09 07:52:49 +0200

    xdfgdf

--- pyCubeStats.py
+++ pyCubeStats.py
@@ -2,7 +2,7 @@ from time import sleep, process_time
 from collections import OrderedDict
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon
-import csv, sys
+import csv, sys, threading
 
 #The inspection time in seconds
 #Change it to whatever suits you
@@ -12,24 +12,29 @@ inspectionTime = 15
 #Change it to whatever suits you
 timeKeys = [60, 55, 50, 45, 40, 35, 30, 25, 20, 15, 10, 5]
 
+#Writes the passed value to the times file
 def writeLatestTime(time):
 	with open("times.txt", "a") as times:
 		times.write(str(time) + ",")
 
+#Reads the times file and returns the times as a list
 def readTimes():
 	times = [time for time in list(csv.reader(open("times.csv", "r")))[0]]
 	times.pop()
 	times = [float(time) for time in times]
 	return times
 
+#Starts the timer and waits for an interrupt
 def runTimer():
 	start = process_time()
-	input()
+	timerDisplay.display(1)
 	return round(process_time() - start, 3)
 
+#Pauses the program for the specified amount of time
 def inspection(inspectionTime):
 	sleep(inspectionTime)
 
+#Calculates the statistics
 def stats(times, timeKeys):
 	#Based on code by https://www.reddit.com/user/yovliporat
 	#https://drive.google.com/file/d/0B7qI7oJsiTPGcjY2VlpoQi1hLVU/view
@@ -47,27 +52,26 @@ def stats(times, timeKeys):
 	for key, val in dictonary.items():
 		print("Sub-{}: {} solves [{}%]".format(str(key), str(val), str(float(val) / float(len(times)) * 100.0)[:5]))
 
-class init(QWidget):
+class Window(QWidget):
 	def __init__(self):
 		super().__init__()
-		self.initUI()
-		
-	def initUI(self):
 		
 		startButton = QPushButton("Start", self)
-		timer = QLCDNumber(self)
+		startButton.clicked.connect(runTimer)
+		timerDisplay = QLCDNumber(self)
 		
 		vbox = QVBoxLayout()
-		vbox.addWidget(timer)
+		vbox.addWidget(timerDisplay)
 		vbox.addWidget(startButton)
 		
 		self.setLayout(vbox)
 		
-		self.setGeometry(300, 300, 250, 100)
+		self.setGeometry(300, 300, 250, 400)
 		self.setWindowTitle("Python Cube Timer")
 		self.setWindowIcon(QIcon("icon.png"))
 		self.show()
+		
 
-app = QApplication(sys.argv)
-initiation = init()
-sys.exit(app.exec_())
+application = QApplication(sys.argv)
+window = Window()
+sys.exit(application.exec_())
