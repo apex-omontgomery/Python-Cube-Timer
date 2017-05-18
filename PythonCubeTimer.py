a384b14d096d45f662040a3d8d88240ad827649a
Storbod <oscar_englundh@hotmail.com>
2017-05-18 13:07:25 +0200

    Added config file and made some changes to the style of the code

--- PythonCubeTimer.py
+++ PythonCubeTimer.py
@@ -4,24 +4,27 @@ from collections import OrderedDict
 from math import ceil, floor
 import csv, os
 
-#Clears the console
-clear = lambda: os.system("cls" if os.name == "nt" else "clear")
-
 #The inspection time in seconds
 #Change it to whatever suits you
 #inspectionTime = 15
 
-#The time keys
+
+#The time keys for displaying sub-x stats
 #Change it to whatever suits you
 timeKeys = [35, 30, 25, 24, 23, 22, 21, 20]
 
-#Writes the passed value to the times file
+
+#Clears the console
+def clear():
+	os.system("cls" if os.name == "nt" else "clear")
+
+
 def writeTime(time):
 	with open("times.csv", "a", newline="") as times:
 		writer = csv.writer(times)
 		writer.writerow([time, "{:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())])
-		
-#Reads the times file and returns the times as a list
+
+
 def readTimes():
 	times, timestamps = [], []
 	with open("times.csv", newline = "") as saveFile:
@@ -33,7 +36,7 @@ def readTimes():
 	times = [float(time) for time in times]
 	return times, timestamps
 
-#Starts the timer and waits for an interrupt
+
 def runTimer():
 	print("Press enter to start timer")
 	input()
@@ -43,19 +46,30 @@ def runTimer():
 	end =  perf_counter()
 	return round(end - start, 3)
 
+
 #Pauses the program for the specified amount of time
 #def inspection(inspectionTime):
 #	sleep(inspectionTime)
 
-#Calculates the statistics
+
 def stats(times, timeKeys, timestamps):
 	#Based on code by https://www.reddit.com/user/yovliporat
 	#https://drive.google.com/file/d/0B7qI7oJsiTPGcjY2VlpoQi1hLVU/view
 	dictonary = OrderedDict()
-	latestTime, last3Times, last5Times, last12Times, last50Times, last100Times = times[-1], times[-3:], times[-5:], times[-12:], times[-50:], times[-100:]
+	latestTime = times[-1]
+	last3Times = times[-3:]
+	last5Times = times[-5:]
+	last12Times = times[-12:]
+	last50Times = times[-50:]
+	last100Times = times[-100:]
 	last12Times.pop(last12Times.index(max(last12Times)))
 	last12Times.pop(last12Times.index(min(last12Times)))
-	totalTime, sumLast3Times, sumLast5Times, sumLast12Times, sumLast50Times, sumLast100Times = sum(times), sum(last3Times), sum(last5Times), sum(last12Times), sum(last50Times), sum(last100Times)
+	totalTime = sum(times)
+	sumLast3Times = sum(last3Times)
+	sumLast5Times = sum(last5Times)
+	sumLast12Times = sum(last12Times)
+	sumLast50Times = sum(last50Times)
+	sumLast100Times = sum(last100Times)
 	sortedTimes = times
 	sortedTimes.sort()
 	
@@ -74,42 +88,34 @@ def stats(times, timeKeys, timestamps):
 		mean = sortedTimes[floor(len(sortedTimes) / 2)] + sortedTimes[ceil(len(sortedTimes) / 2)] / 2
 	
 	clear()
-	print("\nSolves:   " + str(len(times)))
+	print("\nSolves: " + str(len(times)))
 	for key, val in dictonary.items():
 		print("Sub-{}: {} [{}%]".format(str(key), str(val), str(float(val) / float(len(times)) * 100.0)[:5]))
 	
-	print("Ao3:      " + str(round(sumLast3Times / len(last3Times), 3)))
-	print("Ao5:      " + str(round(sumLast5Times / len(last5Times), 3)))
-	print("Ao12:     " + str(round(sumLast12Times / len(last12Times), 3)))
-	print("Ao50:     " + str(round(sumLast50Times / len(last50Times), 3)))
-	print("Ao100:    " + str(round(sumLast100Times / len(last100Times), 3)))
-	print("Average:  " + str(round(totalTime / len(times), 3)))
-	print("Mean:     " + str(mean))
-	print("Best:     " + str(min(times)))
-	print("Worst:    " + str(max(times)))
-	print("Latest:   " + str(latestTime))
+	print("Ao3: " + str(round(sumLast3Times / len(last3Times), 3)))
+	print("Ao5: " + str(round(sumLast5Times / len(last5Times), 3)))
+	print("Ao12: " + str(round(sumLast12Times / len(last12Times), 3)))
+	print("Ao50: " + str(round(sumLast50Times / len(last50Times), 3)))
+	print("Ao100: " + str(round(sumLast100Times / len(last100Times), 3)))
+	print("Average: " + str(round(totalTime / len(times), 3)))
+	print("Mean: " + str(mean))
+	print("Best: " + str(min(times)))
+	print("Worst: " + str(max(times)))
+	print("Latest: " + str(latestTime))
 
 while True:
 	try:
 		times, timestamps = readTimes()
-	except FileNotFoundError:
-		print("Error, file \"times.csv\n not found.")
-	except Exception as e:
-		print("Error: " + str(e))
-		
-	try:
 		stats(times, timeKeys, timestamps)
 	except IndexError:
 		print("Error, no recorded solves.")
+	except FileNotFoundError:
+		print("Error, file \"times.csv\n not found.")
 	except Exception as e:
 		print("Error: " + str(e))
 	
 	try:
 		time = runTimer()
-	except Exception as e:
-		print("Error: " + str(e))
-	
-	try:
 		writeTime(time)
 	except Exception as e:
 		print("Error: " + str(e))
--- createPlot.py
+++ createPlot.py
@@ -6,7 +6,7 @@ print("Done!\nImporting csv...")
 import csv
 print("Done!")
 
-#Reads the times file and returns the times as a list
+
 def readTimes():
 	times = []
 	with open("times.csv", newline = "") as saveFile:
@@ -17,43 +17,25 @@ def readTimes():
 	times = [float(time) for time in times]
 	return times
 
+
 def plot(times, slope, intercept):
-	
-	#Setting the size to 16 * 9 inches
 	pyplot.rcParams["figure.figsize"] = (16, 9)
-	
 	print("Plotting...")
-	
-	#Plotting the best and worst times
 	pyplot.scatter(times.index(max(times)), max(times), color = "r", lw = 3.0)
 	pyplot.scatter(times.index(min(times)), min(times), color = "r", lw = 3.0)
-	
-	#Annotating the best and worst times
 	pyplot.annotate("Worst time: " + str(max(times)), xy = (times.index(max(times)), max(times)), xytext = (times.index(max(times)) + times.index(max(times)) / 100, max(times) + max(times) / 100))
 	pyplot.annotate("Best time: " + str(min(times)), xy = (times.index(min(times)), min(times)), xytext = (times.index(min(times)), min(times) - min(times) / 30))
-	
-	#Plotting all the times
 	pyplot.plot(range(len(times)), times, lw = 2.0, color = "#0099ff")
-	
-	#Creating a trend slope
 	trend = [slope * i + intercept for i in [-1, len(times)]]
 	pyplot.plot([-1, len(times)], trend, lw = 2.0, color = "r", label = "Trend")
-	
-	#Setting the axis sizes
 	pyplot.axis([-1, len(times), min(times) - (min(times) / 10), max(times) + (max(times) / 10)])
-	
-	#Labeling the axis
 	pyplot.xlabel("Solves")
 	pyplot.ylabel("Seconds")
-	
 	print("Done!\nSaving...")
-	
-	#Saving the image
 	pyplot.savefig("plot.png", bbox_inches = "tight", dpi = 200)
-	
 	print("Done!")
-	
 	#pyplot.show()
+
 
 times = readTimes()
 slope, intercept, r_value, p_value, std_err = linregress(range(len(times)), times)
