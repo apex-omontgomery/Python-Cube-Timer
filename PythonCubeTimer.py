cd9c3cf0fed92a93ac6c02f9a7e0e27ebd0aeb35
Storbod <oscar_englundh@hotmail.com>
2017-06-09 18:21:58 +0200

    Made a lot of progress on the GUI

--- PythonCubeTimer.py
+++ PythonCubeTimer.py
@@ -10,16 +10,6 @@ from tkinter import ttk
 from time import sleep, perf_counter
 
 
-def getConfig():
-	configValues = dict()
-	config = configparser.ConfigParser()
-	config.read("config.ini")
-	for key in config["DEFAULT"]:
-		configValues[key] = config["DEFAULT"][key]
-	
-	return configValues
-
-
 def writeTime(time):
 	with open("times.csv", "a", newline="") as times:
 		writer = csv.writer(times)
@@ -37,13 +27,16 @@ def readTimes():
 	return times
 
 
-class GUI():
+class application():
 	def __init__(self):
 		self.root = Tk()
 		self.root.title("Cubing Timer")
+		self.root.option_add("*tearOff", False)
 		self.initGUI()
+		self.root.config(menu = self.menuBar)
+		self.root.resizable(0, 0)
 		self.root.mainloop()
-		
+	
 	
 	def calcStats(self, times):
 		self.solves.set("Solves: " + str(len(times)))
@@ -81,7 +74,7 @@ class GUI():
 		if len(sortedTimes) % 2 == 0:
 			median = round(sortedTimes[ceil(len(sortedTimes) / 2)], 3)
 		else:
-			median = round(sortedTimes[floor(len(sortedTimes) / 2)] + sortedTimes[ceil(len(sortedTimes) / 2)] / 2, 3)
+			median = round((sortedTimes[floor(len(sortedTimes) / 2)] + sortedTimes[ceil(len(sortedTimes) / 2)]) / 2, 3)
 	
 		self.median.set("Median: " + str(median))
 	
@@ -98,11 +91,40 @@ class GUI():
 		self.latest.set("Latest: " + str(times[-1]))
 	
 	
+	def updateTimer(self):
+		self.elapsedTime = int(floor(perf_counter() - self.start))
+		self.minutes, self.seconds = divmod(self.elapsedTime, 60)
+		self.timerLabel.configure(text = "%02d:%02d" % (self.minutes, self.seconds))
+		self.root.after(1000, self.updateTimer)
+	
+	
+	def updateImage(self, times):
+		plot.plot(times)
+		self.image = PhotoImage(file = "plot.png")
+		self.plotImage.configure(image = self.image)
+	
+	
+	def timer(self):
+		self.start = perf_counter()
+		self.timerWindow = Toplevel(self.root)
+		self.timerWindow.title("Timer")
+		self.timerWindow.resizable(0, 0)
+		
+		self.timerLabel = ttk.Label(self.timerWindow, text = "00:00", font = ("Courier", 64))
+		self.timerLabel.grid(column = 0, row = 0, sticky = (N, E, S, W))
+		
+		self.startButton = ttk.Button(self.timerWindow, text = "START", width = 40, command = self.updateTimer)
+		self.startButton.grid(column = 0, row = 1, sticky = (S))
+	
+
 	def initGUI(self):
 		self.mainframe = ttk.Frame(self.root, padding = "3 3 3 3")
 		self.mainframe.grid(column = 0, row = 0, sticky = (N, W, E, S))
 		self.mainframe.columnconfigure(0, weight = 1)
 		self.mainframe.rowconfigure(0, weight = 1)
+		
+		self.menuBar = Menu(self.root)
+		self.menuBar.add_command(label = "Quit", command = self.root.quit)
 		
 		self.solves = StringVar()
 		self.ao3 = StringVar()
@@ -118,24 +140,49 @@ class GUI():
 		self.latest = StringVar()
 		self.standardDeviation = StringVar()
 		
-		self.solvesLabel = ttk.Label(self.mainframe, textvariable = self.solves).grid(column = 1, row = 1, sticky = (E))
-		self.ao3Label = ttk.Label(self.mainframe, textvariable = self.ao3).grid(column = 1, row = 2, sticky = (E))
-		self.ao5Label = ttk.Label(self.mainframe, textvariable = self.ao5).grid(column = 1, row = 3, sticky = (E))
-		self.ao12Label = ttk.Label(self.mainframe, textvariable = self.ao12).grid(column = 1, row = 4, sticky = (E))
-		self.ao50Label = ttk.Label(self.mainframe, textvariable = self.ao50).grid(column = 1, row = 5, sticky = (E))
-		self.ao100Label = ttk.Label(self.mainframe, textvariable = self.ao100).grid(column = 1, row = 6, sticky = (E))
-		self.ao1000Label = ttk.Label(self.mainframe, textvariable = self.ao1000).grid(column = 1, row = 7, sticky = (E))
-		self.averageLabel = ttk.Label(self.mainframe, textvariable = self.average).grid(column = 1, row = 8, sticky = (E))
-		self.medianLabel = ttk.Label(self.mainframe, textvariable = self.median).grid(column = 1, row = 9, sticky = (E))
-		self.standardDeviationLabel = ttk.Label(self.mainframe, textvariable = self.standardDeviation).grid(column = 1, row = 10, sticky = (E))
-		self.bestLabel = ttk.Label(self.mainframe, textvariable = self.best).grid(column = 1, row = 11, sticky = (E))
-		self.worstLabel = ttk.Label(self.mainframe, textvariable = self.worst).grid(column = 1, row = 12, sticky = (E))
-		self.latestLabel = ttk.Label(self.mainframe, textvariable = self.latest).grid(column = 1, row = 13, sticky = (E))
-		
-		for child in self.mainframe.winfo_children(): child.grid_configure(padx = 2, pady = 2)
+		self.solvesLabel = ttk.Label(self.mainframe, textvariable = self.solves)
+		self.solvesLabel.grid(column = 1, row = 1, sticky = (E))
+		self.ao3Label = ttk.Label(self.mainframe, textvariable = self.ao3)
+		self.ao3Label.grid(column = 1, row = 2, sticky = (E))
+		self.ao5Label = ttk.Label(self.mainframe, textvariable = self.ao5)
+		self.ao5Label.grid(column = 1, row = 3, sticky = (E))
+		self.ao12Label = ttk.Label(self.mainframe, textvariable = self.ao12)
+		self.ao12Label.grid(column = 1, row = 4, sticky = (E))
+		self.ao50Label = ttk.Label(self.mainframe, textvariable = self.ao50)
+		self.ao50Label.grid(column = 1, row = 5, sticky = (E))
+		self.ao100Label = ttk.Label(self.mainframe, textvariable = self.ao100)
+		self.ao100Label.grid(column = 1, row = 6, sticky = (E))
+		self.ao1000Label = ttk.Label(self.mainframe, textvariable = self.ao1000)
+		self.ao1000Label.grid(column = 1, row = 7, sticky = (E))
+		self.averageLabel = ttk.Label(self.mainframe, textvariable = self.average)
+		self.averageLabel.grid(column = 1, row = 8, sticky = (E))
+		self.medianLabel = ttk.Label(self.mainframe, textvariable = self.median)
+		self.medianLabel.grid(column = 1, row = 9, sticky = (E))
+		self.standardDeviationLabel = ttk.Label(self.mainframe, textvariable = self.standardDeviation)
+		self.standardDeviationLabel.grid(column = 1, row = 10, sticky = (E))
+		self.bestLabel = ttk.Label(self.mainframe, textvariable = self.best)
+		self.bestLabel.grid(column = 1, row = 11, sticky = (E))
+		self.worstLabel = ttk.Label(self.mainframe, textvariable = self.worst)
+		self.worstLabel.grid(column = 1, row = 12, sticky = (E))
+		self.latestLabel = ttk.Label(self.mainframe, textvariable = self.latest)
+		self.latestLabel.grid(column = 1, row = 13, sticky = (E))
 		
 		times = readTimes()
+		plot.plot(times, 1, 1, 1)
+		self.image = PhotoImage(file = "plot.png")
+		self.plotImage = ttk.Label(self.mainframe, image = self.image)
+		self.plotImage.grid(column = 2, row = 1, rowspan = 13, sticky = (E))
+		
+		self.lowerFrame = ttk.Frame(self.mainframe, padding = "3 3 3 3")
+		self.lowerFrame.grid(column = 1, columnspan = 2, row = 14, sticky = (N, S))
+		
+		self.timerButton = ttk.Button(self.lowerFrame, text = "TIMER", width = 160, command = self.timer)
+		self.timerButton.grid(column = 0, row = 0, sticky = (W, E))
+		
+		for child in self.mainframe.winfo_children():
+			child.grid_configure(padx = 2, pady = 2)
+		
 		self.calcStats(times)
 	
 	
-gui = GUI()
+application = application()
--- plot.py
+++ plot.py
@@ -1,23 +1,37 @@
 import matplotlib.pyplot as pyplot
-from scipy.stats import linregress
 import csv
+try:
+	from scipy.stats import linregress
+	scipyExists = True
+except:
+	scipyExists = False
 
-
-def plot(times):
-	slope, intercept, r_value, p_value, std_err = linregress(range(len(times)), times)
-	pyplot.rcParams["figure.figsize"] = (20, 10)
-	print("Plotting...")
-	pyplot.scatter(times.index(max(times)), max(times), color = "r", lw = 4.0)
-	pyplot.scatter(times.index(min(times)), min(times), color = "r", lw = 4.0)
-	pyplot.annotate("Worst time: " + str(max(times)), xy = (times.index(max(times)), max(times)), xytext = (times.index(max(times)) + times.index(max(times)) / 100, max(times) + max(times) / 100))
-	pyplot.annotate("Best time: " + str(min(times)), xy = (times.index(min(times)), min(times)), xytext = (times.index(min(times)), min(times) - min(times) / 15))
+	
+def plot(times, doTrend = 1, doBest = 1, doWorst = 1):
+	pyplot.cla()
+	pyplot.figure(figsize = (10, 5))
+	
+	#Worst time
+	if doWorst == 1:
+		pyplot.scatter(times.index(max(times)), max(times), color = "r", lw = 4.0)
+		pyplot.annotate("Worst time: " + str(max(times)), xy = (times.index(max(times)), max(times)), xytext = (times.index(max(times)) + times.index(max(times)) / 100, max(times) + max(times) / 100))
+	
+	#Best time
+	if doBest == 1:
+		pyplot.scatter(times.index(min(times)), min(times), color = "r", lw = 4.0)
+		pyplot.annotate("Best time: " + str(min(times)), xy = (times.index(min(times)), min(times)), xytext = (times.index(min(times)), min(times) - min(times) / 15))
+	
+	#All the times
 	pyplot.plot(range(len(times)), times, lw = 2.0, color = "#0099ff", label = "Times")
-	trend = [slope * i + intercept for i in [-1, len(times)]]
-	pyplot.plot([-1, len(times)], trend, lw = 2.0, color = "r", label = "Trend")
+	
+	#Trend
+	if scipyExists and doTrend == 1:
+		slope, intercept, r_value, p_value, std_err = linregress(range(len(times)), times)
+		trend = [slope * i + intercept for i in [0 - len(times) / 15, len(times) + len(times) / 15]]
+		pyplot.plot([0 - len(times) / 15, len(times) + len(times) / 15], trend, lw = 2.0, color = "r", label = "Trend")
+	
 	pyplot.legend(loc = 2, borderaxespad = 1)
-	pyplot.axis([-1, len(times), min(times) - (min(times) / 10), max(times) + (max(times) / 10)])
+	pyplot.axis([0 - len(times) / 15, len(times) + len(times) / 15, min(times) - (min(times) / 10), max(times) + (max(times) / 10)])
 	pyplot.xlabel("Solves")
 	pyplot.ylabel("Seconds")
-	print("Done!\nSaving...")
-	pyplot.savefig("plot.png", bbox_inches = "tight", dpi = 200)
-	print("Done!")
+	pyplot.savefig("plot.png", bbox_inches = "tight", dpi = 100)
